d6cecd452238b51bb5990c7cdf0b98998ae7d370
aptsharp <afonso.raffael@gmail.com>
2023-03-30 22:38:26 -0300

    fix: configuração de chamada do serviço

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,18 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+console.log(document.getElementById("pokemonList"));
+
 fetch(url)
-.then(function (response) {
-  console.log(response);
-})
-.catch(function (error){
-  console.log(error)
-})
-.finally( function (){
-  console.log('concluido') //finaliza requisição da chamada
-})
+  .then((response) => response.json()) //volta uma promise de N
+  .then((jsonBody) => jsonBody.results) // volta o results - que é a lista de Pokémon's
+  .then((pokemonList) => {
+    for (let i = 0; i < pokemonList.length; i++) {
+      const pokemon = pokemonList[i];
+      pokemonList.innerHTML == convertPokemonTali(pokemon);
+    }
+  })
+  //encadeamento de then's pode ser possível transformar a chamada.
+  .catch(function (error) {
+    console.log(error);
+  });
---
+++ assets/js/poke-api.js
@@ -0,0 +1,11 @@
+const pokeApi = {};
+
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+  fetch(url)
+  .then(response => response.json()) //volta uma promise de N
+  .then((jsonBody) => jsonBody.results) // volta o results - que é a lista de Pokémon's
+  .catch()
+};
+
+// ser dono da plataforma! contra o jogo
